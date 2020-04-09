6315586e54aaf40e7ce1dfe7b425bed250859abf
Artem Sydorenko <artem.sydorenko@gmail.com>
2020-04-09 12:59:55 +0300

    Fix access control for AttribitedString

--- Sources/EasySwift/AttributedString.swift
+++ Sources/EasySwift/AttributedString.swift
@@ -8,15 +8,15 @@
 import UIKit
 
 
-typealias AttributedString = NSMutableAttributedString
+public typealias AttributedString = NSMutableAttributedString
 
 extension AttributedString {
     
-    convenience init(_ str: String = "") {
+    public convenience init(_ str: String = "") {
         self.init(string: str)
     }
     
-    func append(_ image: UIImage, inset: UIEdgeInsets = .zero) -> Self {
+    public func append(_ image: UIImage, inset: UIEdgeInsets = .zero) -> Self {
         let attachment = NSTextAttachment()
         attachment.image = image
         if inset != .zero {
@@ -26,22 +26,22 @@ extension AttributedString {
         return self
     }
     
-    func with(_ attribute: NSAttributedString.Key, _ value: Any, _ range: NSRange? = nil) -> Self {
+    public func with(_ attribute: NSAttributedString.Key, _ value: Any, _ range: NSRange? = nil) -> Self {
         addAttributes([attribute: value], range: range ?? NSRange(location: 0, length: length))
         return self
     }
     
-    func with(color: UIColor) -> Self {
+    public func with(color: UIColor) -> Self {
         with(.foregroundColor, color)
     }
-    func with(font: UIFont) -> Self {
+    public func with(font: UIFont) -> Self {
         with(.font, font)
     }
-    func with(link: String, for text: String) -> Self {
+    public func with(link: String, for text: String) -> Self {
         with(.link, link, mutableString.range(of: text))
     }
 }
 
-func + (l: AttributedString, r: AttributedString) -> AttributedString {
+public func + (l: AttributedString, r: AttributedString) -> AttributedString {
     AttributedString(attributedString: l) ~ { $0.append(r) }
 }
