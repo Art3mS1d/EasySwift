d20b20732b4b5f364430382f2b085925db08e6f5
Artem Sydorenko <artem.sydorenko@gmail.com>
2020-04-14 12:24:11 +0300

    Made UserDefaults wrapper value safe and key-typed

--- Sources/EasySwift/UserDefault.swift
+++ Sources/EasySwift/UserDefault.swift
@@ -9,12 +9,12 @@ import Foundation
 
 
 @propertyWrapper
-public struct UserDefault<Value> {
-    let key: String
+public struct UserDefault<Value: PropertyListValue> {
+    let key: UserDefaultKey
     let defaultValue: Value
     let defaults: UserDefaults
 
-    public init(wrappedValue: Value, _ key: String, _ defaults: UserDefaults = .standard) {
+    public init(wrappedValue: Value, _ key: UserDefaultKey, _ defaults: UserDefaults = .standard) {
         self.key = key
         self.defaultValue = wrappedValue
         self.defaults = defaults
@@ -22,17 +22,45 @@ public struct UserDefault<Value> {
 
     public var wrappedValue: Value {
         get {
-            defaults.object(forKey: key) as? Value ?? defaultValue
+            defaults.object(forKey: key.rawValue) as? Value ?? defaultValue
         }
         set {
             if let value = newValue as? OptionalProtocol, value.isNil {
-                defaults.removeObject(forKey: key)
+                defaults.removeObject(forKey: key.rawValue)
             } else {
-                defaults.set(newValue, forKey: key)
+                defaults.set(newValue, forKey: key.rawValue)
             }
         }
     }
 }
+
+
+public struct UserDefaultKey: RawRepresentable, ExpressibleByStringLiteral {
+    public let rawValue: String
+    public init(rawValue: String) {
+        self.rawValue = rawValue
+    }
+    public init(stringLiteral: String) {
+        rawValue = stringLiteral
+    }
+}
+
+
+public protocol PropertyListValue {}
+
+extension Data: PropertyListValue {}
+extension String: PropertyListValue {}
+extension Date: PropertyListValue {}
+extension Bool: PropertyListValue {}
+extension Int: PropertyListValue {}
+extension Double: PropertyListValue {}
+extension Float: PropertyListValue {}
+
+extension Array: PropertyListValue where Element: PropertyListValue {}
+extension Dictionary: PropertyListValue where Key == String, Value: PropertyListValue {}
+
+extension Optional: PropertyListValue where Wrapped: PropertyListValue {}
+
 
 fileprivate protocol OptionalProtocol {
     var isNil: Bool { get }
@@ -43,4 +71,3 @@ extension Optional: OptionalProtocol {
         self == nil
     }
 }
-
---
+++ Tests/EasySwiftTests/UserDefaultTests.swift
@@ -0,0 +1,43 @@
+//
+//  UserDefaultTests.swift
+//  EasySwiftTests
+//
+//  Created by Artem Sydorenko on 14.04.2020.
+//
+
+import XCTest
+@testable import EasySwift
+
+
+class UserDefaultTests: XCTestCase {
+    
+    @UserDefault("test1")
+    var default1 = "morning"
+    
+    func testSimple() {
+        default1 = "night"
+        XCTAssertEqual(default1, "night")
+    }
+    
+    @UserDefault(wrappedValue: nil, "test2")
+    var default2: Date?
+    
+    func testOptional() {
+        XCTAssertNil(default2)
+        default2 = Date()
+        XCTAssertNotNil(default2)
+        default2 = nil
+        XCTAssertNil(default2)
+    }
+    
+    @UserDefault(.isFirstLaunch)
+    var default3 = true
+
+    func testKeys() {
+        default3 = false
+    }
+}
+
+extension UserDefaultKey {
+    static let isFirstLaunch: Self = "isFirstLaunch"
+}
